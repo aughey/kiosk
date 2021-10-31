7e502f961ddab96f95c33166069046a6cf2d45cb
John Aughey <jha@aughey.com>
2021-10-31 16:30:13 -0500

    foo

---
+++ kiosk.sh
@@ -0,0 +1,12 @@
+export DISPLAY=:0
+
+xset s off
+xset -dpms
+
+while [ 1 ]; do
+  killall -w unclutter
+  killall -w chromium-browser
+  unclutter &
+  chromium-browser -kiosk 'https://docs.google.com/presentation/d/e/2PACX-1vQms_iW5THtj9sQPx1PPzueQofifFHnoA8UM0G97XmyTXnrcoSAMW9Bd_2NptybVkqYLpYHRhpWGYAa/pub?start=true&loop=true&delayms=10000'
+done
+
