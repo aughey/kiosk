98d0941894ba1df53348872cf4feb5a2e6b3ce89
John Aughey <jha@aughey.com>
2021-10-31 16:53:56 -0500

    using temp profile

--- kiosk.sh
+++ kiosk.sh
@@ -7,6 +7,6 @@ while [ 1 ]; do
   killall -w unclutter
   killall -w chromium-browser
   unclutter &
-  chromium-browser -kiosk 'https://docs.google.com/presentation/d/e/2PACX-1vQms_iW5THtj9sQPx1PPzueQofifFHnoA8UM0G97XmyTXnrcoSAMW9Bd_2NptybVkqYLpYHRhpWGYAa/pub?start=true&loop=true&delayms=10000'
+  chromium-browser --temp-profile -kiosk 'https://docs.google.com/presentation/d/e/2PACX-1vQms_iW5THtj9sQPx1PPzueQofifFHnoA8UM0G97XmyTXnrcoSAMW9Bd_2NptybVkqYLpYHRhpWGYAa/pub?start=true&loop=true&delayms=10000'
 done
 
