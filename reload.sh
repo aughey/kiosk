443fe0c67eacd8358b3edba32408eeea716530a2
John Aughey <jha@aughey.com>
2021-11-01 15:40:07 -0500

    foo

--- reload.sh
+++ reload.sh
@@ -1,7 +1,9 @@
 . ./env
 while [ 1 ]; do
+	echo "waiting for message"
 	reload=`mosquitto_sub -h $MQTT_HOST -t reload -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -C 1`
 	if [ "$reload" = "reload" ]; then
+		echo "reloading"
 		DISPLAY=:0 xdotool search --onlyvisible --class Chrome windowfocus key ctrl+r
 	fi
 	sleep 1
