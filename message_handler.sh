cfa4acd4f59130271742c90e12a3476ce4b5f43f
John Aughey <jha@aughey.com>
2021-11-01 17:18:55 -0500

    foo

--- message_handler.sh
+++ message_handler.sh
@@ -7,12 +7,12 @@ sendmsg()
 
 
 HOSTNAME=`hostname`
-sendmsg("$HOSTNAME listening for commands")
+sendmsg "$HOSTNAME listening for commands"
 while [ 1 ]; do
 	#echo "waiting for message"
 	command=`mosquitto_sub -h $MQTT_HOST -t command -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -C 1`
 	if [ "$command" = "reload" ]; then
-		sendmsg("reloading")
+		sendmsg "reloading"
 		#echo "reloading"
 		DISPLAY=:0 xdotool search --onlyvisible --class Chrome windowfocus key ctrl+r
 	fi
