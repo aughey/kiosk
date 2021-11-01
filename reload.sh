d7831870e1fe271e7a39c1a6119cbc217f855daa
John Aughey <jha@aughey.com>
2021-11-01 15:39:09 -0500

    foo

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
 . ./env
 while [ 1 ]; do
-	reload=`mosquitto_sub -h www.washucsc.org -t reload -p 3001 -u $MQTT_USER -P $MQTT_PASSWORD -C 1`
+	reload=`mosquitto_sub -h $MQTT_HOST -t reload -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -C 1`
 	if [ "$reload" = "reload" ]; then
 		DISPLAY=:0 xdotool search --onlyvisible --class Chrome windowfocus key ctrl+r
 	fi
