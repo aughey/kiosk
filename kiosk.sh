3799a27b76734970c9394e7349e6863287d31eb9
John Aughey <jha@aughey.com>
2021-11-01 14:55:19 -0500

    udpating url

--- kiosk.sh
+++ kiosk.sh
@@ -1,11 +1,13 @@
 export DISPLAY=:0
 
+export URL='https://docs.google.com/presentation/d/e/2PACX-1vRloaV-I9H6O0gGfr2P6PAssAG0tOVbdsQ3lU5MdAksuE0YhPJ757kCX3CREAmB_ummgNU8jycO_XBz/pub?start=true&loop=true&delayms=10000'
+
 sh /home/pi/src/kiosk/keepon.sh &
 
 while [ 1 ]; do
   killall -w unclutter
   killall -w chromium-browser
   unclutter &
-  chromium-browser --temp-profile -kiosk 'https://docs.google.com/presentation/d/e/2PACX-1vQms_iW5THtj9sQPx1PPzueQofifFHnoA8UM0G97XmyTXnrcoSAMW9Bd_2NptybVkqYLpYHRhpWGYAa/pub?start=true&loop=true&delayms=10000'
+  chromium-browser --temp-profile -kiosk $URL
 done
 
