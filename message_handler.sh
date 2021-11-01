3898df35d46a872f43e0e5fb44c248efa4e4df22
John Aughey <jha@aughey.com>
2021-11-01 17:09:49 -0500

    foo

--- message_handler.sh
+++ message_handler.sh
@@ -3,7 +3,7 @@ while [ 1 ]; do
 	#echo "waiting for message"
 	command=`mosquitto_sub -h $MQTT_HOST -t command -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -C 1`
 	if [ "$command" = "reload" ]; then
-		mosquitto_sub -h $MQTT_HOST -t message/$HOSTNAME -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -m "reloading"
+		mosquitto_pub -h $MQTT_HOST -t message/$HOSTNAME -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -m "reloading"
 		#echo "reloading"
 		DISPLAY=:0 xdotool search --onlyvisible --class Chrome windowfocus key ctrl+r
 	fi
