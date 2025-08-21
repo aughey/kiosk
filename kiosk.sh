80f8625943146a609463e9ae32529ab437a492a7
John Aughey <jha@aughey.com>
2025-08-21 06:51:47 -0700

    Updating kiosk url

--- kiosk.sh
+++ kiosk.sh
@@ -3,7 +3,8 @@ export PATH=/usr/local/bin:$PATH
 
 git pull
 
-export URL='https://docs.google.com/presentation/d/e/2PACX-1vRloaV-I9H6O0gGfr2P6PAssAG0tOVbdsQ3lU5MdAksuE0YhPJ757kCX3CREAmB_ummgNU8jycO_XBz/pub?start=true&loop=true&delayms=10000'
+#export URL='https://docs.google.com/presentation/d/e/2PACX-1vRloaV-I9H6O0gGfr2P6PAssAG0tOVbdsQ3lU5MdAksuE0YhPJ757kCX3CREAmB_ummgNU8jycO_XBz/pub?start=true&loop=true&delayms=10000'
+export URL='https://docs.google.com/presentation/d/e/2PACX-1vSdXf8BUPHszW-Jp6gLGYkaTCkkn-9P-EuryZ8Hld46gR_HStOGK3sssdKQCm6BKRABldOdqvjbfH43/pub?start=true&loop=true&delayms=10000'
 
 sh /home/pi/src/kiosk/keepon.sh &
 sh /home/pi/src/kiosk/message_handler.sh &
